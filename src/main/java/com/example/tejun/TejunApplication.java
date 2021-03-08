ad279cdea08611379b6605b96b3691dbe1b80b02
Kyosuke Chiba <chiba-ks@bhopari.onmicrosoft.com>
2021-03-08 15:11:47 +0900

    最初のコミット

---
+++ src/main/java/com/example/tejun/TejunApplication.java
@@ -0,0 +1,13 @@
+package com.example.tejun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TejunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TejunApplication.class, args);
+	}
+
+}
